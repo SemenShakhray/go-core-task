ee9eeab88d03427092c23fa1b8e5d3954ff9a580
Shakhray <shakhrai.semion@yandex.ru>
2024-11-15 12:20:55 +0300

    task1

---
+++ task1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var numDecimal int = 42           // Десятичная система
+	var numOctal int = 052            // Восьмеричная система
+	var numHexadecimal int = 0x2A     // Шестнадцатиричная система
+	var pi float64 = 3.14             // Тип float64
+	var name string = "Golang"        // Тип string
+	var isActive bool = true          // Тип bool
+	var complexNum complex64 = 1 + 2i // Тип complex64
+
+	fmt.Printf("numDecimal: %T\n", numDecimal)
+	fmt.Printf("numOctal: %T\n", numOctal)
+	fmt.Printf("numHexadecimal: %T\n", numHexadecimal)
+	fmt.Printf("pi: %T\n", pi)
+	fmt.Printf("name: %T\n", name)
+	fmt.Printf("isActive: %T\n", isActive)
+	fmt.Printf("complexNum: %T\n", complexNum)
+
+	var str strings.Builder
+	str.WriteString(fmt.Sprint(numDecimal))
+	str.WriteString(fmt.Sprint(numOctal))
+	str.WriteString(fmt.Sprint(numHexadecimal))
+	str.WriteString(fmt.Sprint(pi))
+	str.WriteString(fmt.Sprint(name))
+	str.WriteString(fmt.Sprint(isActive))
+	str.WriteString(fmt.Sprint(complexNum))
+
+	strJoin := str.String()
+	strRune := []rune(strJoin)
+
+	fmt.Println(strJoin, strRune)
+
+	sault := "go-2024"
+	hasher := sha256.New()
+	hasher.Write([]byte(sault + strJoin))
+	fmt.Println(hex.EncodeToString(hasher.Sum(nil)))
+
+}
